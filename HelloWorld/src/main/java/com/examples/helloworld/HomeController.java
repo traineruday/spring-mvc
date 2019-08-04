ad9bd6eab021543d7e46e7f75b179d12c49fa752
Uday <udaykumarjagini@gmail.com>
2019-08-04 16:01:10 +0530

    hello world example

---
+++ HelloWorld/src/main/java/com/examples/helloworld/HomeController.java
@@ -0,0 +1,23 @@
+package com.examples.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "home";
+	}
+
+}
